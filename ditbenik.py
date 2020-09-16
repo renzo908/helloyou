898d7ea1e6c58eaa454783f2b09a5d7d2be08a31
renzo908 <70953384+renzo908@users.noreply.github.com>
2020-09-16 12:03:55 +0200

    renzo hoogewerf SD1BA

---
+++ ditbenik.py
@@ -0,0 +1,65 @@
+print("hallo jij")
+print("ik ben renzo")
+print("wie ben jij")
+print("type je naam in")
+inputnaam = input()
+print("hallo" + inputnaam)
+
+print("wat is mijn lievelings sport?")
+print("A. basketbal")
+print("B. voetbal")
+print("C. honkbal")
+
+
+antwoord = input("A B of C?")
+if antwoord == "A":
+    print("jouw antwoord was "+ antwoord + " dit heb je helaas fout ")
+    
+if antwoord == "B":
+    print("jouw antwoord was "+ antwoord + " dit heb je helemaal goed ")
+
+if antwoord == "C":
+    print("jouw antwoord was "+ antwoord + " dit heb je helaas fout ")
+
+
+
+
+
+print("wat doe ik het liefste in mijn vrije tijd?")
+print("A. voetballen")
+print("B. formule 1 kijken")
+print("C. slapen en eten")
+
+antwoord = input("A B of C?")
+if antwoord == "A":
+     print("jouw antwoord was "+ antwoord + " dit heb je goed ")
+
+if antwoord == "B":
+    print("jouw antwoord was "+ antwoord + " voetbal vindt ik net wat leuker ")
+
+if antwoord == "C":
+    print("jouw antwoord was "+ antwoord + " dit heb je helaas fout ")
+
+
+print("wat is mijn favorite vakantie bestemming?")
+print("A. frankrijk")
+print("B. spanje")
+print("C. italië")
+
+antwoord = input("A B of C?")
+if antwoord == "A":
+    print("jouw antwoord was "+ antwoord + " niet de leukste ")
+
+if antwoord == "B":
+    print("jouw antwoord was "+ antwoord + " helemaal goed ")
+
+if antwoord == "C":
+    print("jouw antwoord was "+ antwoord + " nog nooit geweest ")
+    
+
+
+
+
+
+
+
