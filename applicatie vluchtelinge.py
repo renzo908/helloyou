6b1f75fb6d027f8d4d5183b66ba33fb5bb2c9c44
renzo908 <70953384+renzo908@users.noreply.github.com>
2020-10-28 21:36:21 +0100

    Add files via upload

---
+++ applicatie vluchtelinge.py
@@ -0,0 +1,75 @@
+#vraag 1
+print("vraag 1")
+print("waar kwamen salah en zijn gezin vandaan")
+print("A : hij kwam uit serië")
+print("B : hij kwam uit Saudi-Arabië")
+print("C : hij kwam uit afrika")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is juist")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 2
+print("vraag 2")
+print("wat waren salah en zijn gezin in Saudi-Arabië")
+print("A : ze waren swerfer")
+print("B : ze  werden behandeld als tweederangsburgers")
+print("C : ze hadden een leuk leven")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is juist")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 3
+print("vraag 3")
+print("in welke plaats zitten ze in het azc")
+print("A : in rotterdam")
+print("B : in amsterdam")
+print("C : in alkmaar")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is goed")
+if antwoord "B" :
+    print("dit is fout")
+if antwoord "C" :
+    print("dit is fout")
+
+#vraag 4
+print("vraag 4")
+print("wat wil salah later voor werk doen in nederland")
+print("A : hij wil later met dieren helpen")
+print("B : hij wil later mensen helpen met shulden")
+print("C : hij wil later kinderen met autischme helpen")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is fout")
+if antwoord "B" :
+    print("dit is fout")
+if antwoord "C" :
+    print("dit is goed")
+
+#vraag 5
+print("vraag 5")
+print("hoe heet de dochter van salah")
+print("A : Awadh")
+print("B : shopia")
+print("C : lotte")
+antwoord = input()
+if antwoord == "A" :
+    print("dit is goed")
+if antwoord "B" :
+    print("dit is fout")
+if antwoord "C" :
+    print("dit is fout")
+
+
+
+
+
+   
